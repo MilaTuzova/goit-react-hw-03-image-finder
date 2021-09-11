02809a9668b025f70ebc12e961974e7e7b7f7aab
MilaTuzova <tuzovals@gmail.com>
2021-09-11 11:57:14 +0300

    started

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import { v4 as uuidv4 } from 'uuid';
+
+class App extends Component {
+  state = {};
+
+  
+  render() {
+   
+    return (
+      <div>
+       Hello!!! I'm working!!!
+      </div>
+    );
+  }
+}
+
+export default App;
